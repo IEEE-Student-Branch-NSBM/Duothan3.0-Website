4c5a7e74eab098733e9944cbbf0529cebe3329a9
Arun Chandran <53257113+arunchandran409@users.noreply.github.com>
2023-02-02 23:54:05 +0530

    add faq intio rules

--- components/faq.js
+++ components/faq.js
@@ -55,4 +55,22 @@ const faqdata = [
     answer:
       "No, we don't offer technical support for free downloads. Please purchase a support plan to get 6 months of support.",
   },
+   {
+    question: "What conditions may apply for Duathon 3.0?",
+    answer:
+    " A team should consist of four participants. Team members should belong to the same university. The participants should compete throughout the whole 07 hours of the buildathon. Teams should compete for the competition according to the instructions provided by the organizing committee of Duothan 3.0. Every participant should accept and respect the final decision of the organizing committee. For all the undergraduates from any university."
+  },
+  {
+    question: "What is the flow of the buildathon?",
+    answer: 
+    "The buildathon starts at 9.00 AM on 24th February. You need to build the half-build application according to the given instructions within 07 hours of time."
+  },
+  {
+    question: "Why participate?",
+    answer: "This is a fabulous occasion to pair up with your partners and build the heart out this February. You will get a better experience in developing throughout the session. You will be able to understand your level and start from where you are now and improve yourselves. Hack this valentine with your partner and stand a chance to win cash prizes and certificates. We hope to give a digital certificate for all the participants to appreciate their effort and participation in the Duothan 3.0."
+  },
+  {
+    question: "How to reach us?",
+    answer: "You can reach out to the organizing committee of Duothan 3.0 during the buildathon. If you have any questions, please feel free to contact us ASAP. You can contact us via the IEEE NSBM Facebook page or send an email to nsbmieee@gmail.com."
+  },
 ];
