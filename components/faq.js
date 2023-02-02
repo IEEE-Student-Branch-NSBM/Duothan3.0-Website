75657ddf5e4da3698b49b45ef21bd2ed2049d30b
Prineth Fernando <prinethfernandox@gmail.com>
2023-02-02 15:12:12 +0530

    updates
    faq
    hero
    navbar ids
    rules margins fix
    sponsors title
    timeline
    index

--- components/faq.js
+++ components/faq.js
@@ -5,8 +5,11 @@ import { ChevronUpIcon } from "@heroicons/react/solid";
 
 export default function Faq() {
   return (
-    <Container className="!p-0">
-      <div className="w-full max-w-2xl p-2 mx-auto rounded-2xl">
+    <Container className="flex flex-wrap mt-20">
+       <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full">
+            RULES & REGULATIONS
+          </h1>
+      <div className="w-full mt-8 mx-auto rounded-2xl">
         {faqdata.map((item, index) => (
           <div key={item.question} className="mb-5">
             <Disclosure>
