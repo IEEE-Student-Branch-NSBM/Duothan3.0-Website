1e50443acc63fd0959346d235dae8fe1d434c949
Prineth Fernando <prinethfernandox@gmail.com>
2023-02-03 19:33:08 +0530

    Update faq.js

--- components/faq.js
+++ components/faq.js
@@ -7,37 +7,36 @@ export default function Faq() {
   return (
     <div id="faq">
       <Container className="flex flex-wrap mt-20">
-      <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full">
-        RULES & REGULATIONS
-      </h1>
-      <div className="w-full mt-8 mx-auto rounded-2xl">
-        {faqdata.map((item, index) => (
-          <div key={item.question} className="mb-5">
-            <Disclosure>
-              {({ open }) => (
-                <>
-                  <Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-200 rounded-lg bg-gray-900 hover:bg-gray-focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 ">
-                    <span>{item.question}</span>
-                    <ChevronUpIcon
-                      className={`${
-                        open ? "transform rotate-180" : ""
-                      } w-5 h-5 text-red`}
-                    />
-                  </Disclosure.Button>
-                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-300">
-                    {item.answer.map((ans) => {
-                      return <li>{ans}</li>;
-                    })}
-                  </Disclosure.Panel>
-                </>
-              )}
-            </Disclosure>
-          </div>
-        ))}
-      </div>
-    </Container>
+        <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full">
+          FREQUENTLY ASKED QUESTIONS
+        </h1>
+        <div className="w-full mt-8 mx-auto rounded-2xl">
+          {faqdata.map((item, index) => (
+            <div key={item.question} className="mb-5">
+              <Disclosure>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-200 rounded-lg bg-gray-900 hover:bg-gray-focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 ">
+                      <span>{item.question}</span>
+                      <ChevronUpIcon
+                        className={`${
+                          open ? "transform rotate-180" : ""
+                        } w-5 h-5 text-red`}
+                      />
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-300">
+                      {item.answer.map((ans) => {
+                        return <li>{ans}</li>;
+                      })}
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            </div>
+          ))}
+        </div>
+      </Container>
     </div>
-    
   );
 }
 
