174130e4771b910d274d1f6fb05ba7a7ffcf8c43
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2023-02-22 22:57:49 +0530

    Edit #

--- components/testimonials.js
+++ components/testimonials.js
@@ -14,8 +14,8 @@ export default function Testimonials() {
         <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full">
           CASH PRIZES
         </h1>
-        <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3 mt-10 [&>*:nth-child(2)]:order-1 xl:[&>*:nth-child(2)]:order-2">
-          <div className="lg:col-span-2 xl:col-auto xl:self-end">
+        <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3 mt-10">
+          <div className="lg:col-span-2 xl:col-auto xl:self-end order-2 xl:order-1">
             <div className="flex flex-col justify-between w-full h-full bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
 
               <Avatar
