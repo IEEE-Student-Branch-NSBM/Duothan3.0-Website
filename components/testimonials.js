fcae4e79dbf3d629ad70eefc58f9ca93aa52cfdd
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2023-02-22 22:47:53 +0530

    edit

--- components/testimonials.js
+++ components/testimonials.js
@@ -11,10 +11,10 @@ export default function Testimonials() {
   return (
     <Container>
       <div className="w-full">
-        <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full mb-4">
+        <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full">
           CASH PRIZES
         </h1>
-        <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3 ">
+        <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3 mt-10 [&>*:nth-child(2)]:order-1 xl:[&>*:nth-child(2)]:order-2">
           <div className="lg:col-span-2 xl:col-auto xl:self-end">
             <div className="flex flex-col justify-between w-full h-full bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
 
