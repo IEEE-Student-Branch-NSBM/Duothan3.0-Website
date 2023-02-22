7b15c45982038420f607946ca12b5c03929b11db
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2023-02-22 22:31:02 +0530

    adjustment fix

--- components/testimonials.js
+++ components/testimonials.js
@@ -10,38 +10,43 @@ import SecondRunnerUp from "../public/img/SecondRunnerUp.png";
 export default function Testimonials() {
   return (
     <Container>
-      <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="lg:col-span-2 xl:col-auto lg:self-end">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+      <div className="w-full flex flex-col items-center">
+        <h1 className="box-border border-b-2 border-[#E52145] pt-4 pb-0.5 text-center text-2xl font-semibold text-[#E52145] w-full">
+          CASH PRIZES
+        </h1>
+        <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
+          <div className="lg:col-span-2 xl:col-auto lg:self-end">
+            <div className="flex flex-col justify-between w-full h-full bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
 
-            <Avatar
-              image={RunnerUp}
-              name="1st Runner Up"
-              title="Rs.20,000"
-              width={303} height={244.76}
-            />
+              <Avatar
+                image={RunnerUp}
+                name="1st Runner Up"
+                title="Rs.20,000"
+                width={303} height={244.76}
+              />
+            </div>
           </div>
-        </div>
-        <div className="p-0 lg:p-[60px]">
-          <div className="flex flex-col justify-between bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            
+          <div className="pb-0 lg:pb-[60px]">
+            <div className="flex flex-col justify-between bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+              
 
-            <Avatar
-              image={Winner}
-              name="Winner"
-              title="Rs.30,000"
-            />
+              <Avatar
+                image={Winner}
+                name="Winner"
+                title="Rs.30,000"
+              />
+            </div>
           </div>
-        </div>
-        <div className="lg:self-end">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            
+          <div className="lg:self-end">
+            <div className="flex flex-col justify-between w-full h-full bg-gray-800 text-white px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+              
 
-            <Avatar
-              image={SecondRunnerUp}
-              name="2nd Runner Up"
-              title="RS.10,000"
-            />
+              <Avatar
+                image={SecondRunnerUp}
+                name="2nd Runner Up"
+                title="RS.10,000"
+              />
+            </div>
           </div>
         </div>
       </div>
