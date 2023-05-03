e22d327c3b3374241c3deac80a3d83a1c70c81a8
Prineth Fernando <prinethfernandox@gmail.com>
2023-05-03 16:27:03 +0530

    Create index.js

---
+++ pages/cert/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+ <div>
+      <iframe
+      src="https://script.google.com/macros/s/AKfycbzE64RuOhcCDl9tA0q22Z8acAuwiv0-qI-XaBIe9a4hR3IIPOVZ1QbmqRe_MnGykr55jA/exec"
+      style={{ width: "100%", height: "100vh" }}
+    ></iframe>
+ </div>
+  );
+}
+
+export default Home;
